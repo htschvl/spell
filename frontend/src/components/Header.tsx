a2824083f6597e498b0d0e8e178acf16dbba4b3b
Josh <josh_alot@protonmail.com>
2024-12-30 15:12:24 -0300

    implementing the solana components to connect and disconnect wallets

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,18 +1,42 @@
-import '../styles/Header.scss'
+import { Link } from 'react-router-dom';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { clusterApiUrl } from '@solana/web3.js';
+
+import '../styles/Header.scss';
+import '@solana/wallet-adapter-react-ui/styles.css';
 
 function Header() {
+    const endpoint = clusterApiUrl('devnet'); // TODO: implements environment variables
+    const wallets = [new PhantomWalletAdapter()];
+
     return (
-        <header className="spellcasters-header">
-            <img src="logo.png" alt="Logo" className="logo" />
-            <nav>
-                <ul>
-                    <li>NFTs ($SPELL Holders)</li>
-                    <li>Collabs</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
-            <button className="connect-wallet">Connect Wallet (Coming Soon)</button>
-        </header>
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={wallets} autoConnect>
+                <WalletModalProvider>
+                    <header className="spellcasters-header">
+                        <Link to="/">
+                            <img src="logo.png" alt="Logo" className="logo" />
+                        </Link>
+                        <nav>
+                            <ul>
+                                <li>
+                                    <Link to="/nfts">NFTs ($SPELL Holders)</Link>
+                                </li>
+                                <li>
+                                    <Link to="/collabs">Collabs</Link>
+                                </li>
+                                <li>
+                                    <Link to="/contact">Contact</Link>
+                                </li>
+                            </ul>
+                        </nav>
+                        <WalletMultiButton />
+                    </header>
+                </WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
     );
 }
 
