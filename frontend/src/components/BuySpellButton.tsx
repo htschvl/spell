4bf51341abf82116c6d7efb37573a44cc3e95f7e
aquental <antonio.quental@gmail.com>
2025-01-18 00:50:18 -0300

    removi metadata antiga

--- frontend/src/components/BuySpellButton.tsx
+++
@@ -1,9 +0,0 @@
-import '../styles/BuySpellButton.scss';
-
-const BuySpellButton = () => {
-    return (
-        <button className="buy-spell">Buy $SPELL</button>
-    )
-}
-
-export default BuySpellButton;
